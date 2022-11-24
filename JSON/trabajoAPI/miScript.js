8637c7693e702ed902782cbe32771ce8e2afaadc
robertdenism <robert1994denis@gmail.com>
2022-11-24 21:42:08 +0100

    buscar por ID

--- JSON/trabajoAPI/miScript.js
+++ JSON/trabajoAPI/miScript.js
@@ -1,92 +1,94 @@
 
-        const cors ='https://cors-anywhere.herokuapp.com/'
-        const yourUrl =cors+'https://nodejs-mysql-restapi-production-d8bd.up.railway.app/api/employees/';
+const cors = 'https://cors-anywhere.herokuapp.com/'
+const yourUrl = cors + 'https://nodejs-mysql-restapi-production-d8bd.up.railway.app/api/employees/';
 
 
-        function aniadir (){
-            let entrada = document.getElementsByTagName('input');
-            
-            var nombreB = true;
-            var salarioB = true;
+function aniadir() {
+    let entrada = document.getElementsByTagName('input');
 
-            if(entrada[0].value.length < 5 || entrada[0].value.length>40){
-                window.alert("Minimo 5 carácteres y max 40");
-                nombreB=false;
-            }else if(entrada[1].value.length==0 || entrada[1].value.length>99999){
-                window.alert("Introduzca un sueldo válido");
-                salarioB=false;
-            }
-            if(nombreB==true && salarioB==true){
-            var nombre = document.getElementById("inNombre").value;
-            var salario = document.getElementById("inSalario").value;
-                const data ={
-                    name:nombre,
-                    salary:salario
-                }
-            
-                fetch(yourUrl, {method:'POST', body: JSON.stringify(data), headers:{'Content-Type': 'application/json'}})
-                .then(response => response.json())
-                .then(datos => console.log(datos));  
-            }
-        }   
-            
-           
-        
+    var nombreB = true;
+    var salarioB = true;
 
-        function editar() {
-
-            var id = document.getElementById("inId").value;
-            var nombre = document.getElementById("inNombre").value;
-            var salario = document.getElementById("inSalario").value;
-                const data ={
-                    name:nombre,
-                    salary:salario
-                }
-            
-                fetch(yourUrl+id, {method:'PATCH', body: JSON.stringify(data), headers:{'Content-Type': 'application/json'}})
-                .then(response => response.json())
-                .then(datos => console.log(datos));
-
-                
-            }
-
-        function eliminar() {
-            var catchId = document.getElementById("inId").value;
-            fetch(yourUrl+catchId,{method:'DELETE'})
-                .then(response => response.json())
-                .then(datos => (datos));
+    if (entrada[0].value.length < 5 || entrada[0].value.length > 40) {
+        window.alert("Minimo 5 carácteres y max 40");
+        nombreB = false;
+    } else if (entrada[1].value.length == 0 || entrada[1].value.length > 99999) {
+        window.alert("Introduzca un sueldo válido");
+        salarioB = false;
+    }
+    if (nombreB == true && salarioB == true) {
+        var nombre = document.getElementById("inNombre").value;
+        var salario = document.getElementById("inSalario").value;
+        const data = {
+            name: nombre,
+            salary: salario
         }
 
-        function findId() {
-            var catchId = document.getElementById("inId").value;
-            fetch(yourUrl+catchId)
-                .then(response => response.json())
-                .then(datos => tabla2(datos));
-        }
+        fetch(yourUrl, { method: 'POST', body: JSON.stringify(data), headers: { 'Content-Type': 'application/json' } })
+            .then(response => response.json())
+            .then(datos => console.log(datos));
+    }
+}
 
-        function findAll() {
-            fetch(yourUrl)
-                .then(response => response.json())
-                .then(datos => tabla(datos));
-        }
-        
-        function tabla(datos){
-            contenido.innerHTML = '';
-            for(let valor of datos){
-                contenido.innerHTML += `
+
+
+
+function editar() {
+
+    var id = document.getElementById("inId").value;
+    var nombre = document.getElementById("inNombre").value;
+    var salario = document.getElementById("inSalario").value;
+    const data = {
+        name: nombre,
+        salary: salario
+    }
+
+    fetch(yourUrl + id, { method: 'PATCH', body: JSON.stringify(data), headers: { 'Content-Type': 'application/json' } })
+        .then(response => response.json())
+        .then(datos => console.log(datos));
+
+
+}
+
+function eliminar() {
+    var catchId = document.getElementById("inId").value;
+    fetch(yourUrl + catchId, { method: 'DELETE' })
+        .then(response => response.json())
+        .then(datos => (datos));
+}
+
+function findId() {
+    var catchId = document.getElementById("inId").value;
+    fetch(yourUrl + catchId)
+        .then(response => response.json())
+        .then(datos => tabla2(datos));
+}
+
+function findAll() {
+    fetch(yourUrl)
+        .then(response => response.json())
+        .then(datos => tabla(datos));
+}
+
+function tabla(datos) {
+    contenido.innerHTML = '';
+    for (let valor of datos) {
+        contenido.innerHTML += `
 
                     <tr>
-                        <td>${valor.id}</td>
+                        <td><a href="Empleado.html?${valor.id}">${valor.id}</a></td>
                         <td>${valor.name}</td>
                         <td>${valor.salary}</td>
                     </tr>
                 `
-            }
-        }
-        
-        function tabla2(datos){
-            contenido.innerHTML = '';
-            contenido.innerHTML += `
+    }
+}
+
+
+
+function tabla2(datos) {
+    contenido.innerHTML = '';
+    contenido.innerHTML += `
 
                     <tr>
                         <td>${datos.id}</td>
@@ -94,22 +96,22 @@
                         <td>${datos.salary}</td>
                     </tr>
                 `
-        }
+}
 
-        function add2(){
-            let entrada = document.getElementsByTagName('input');
-            
-            var nombreB = true;
-            var salarioB = true;
+function add2() {
+    let entrada = document.getElementsByTagName('input');
 
-            if(entrada[0].value.length < 5 || entrada[0].value.length>40){
-                window.alert("Minimo 5 carácteres y max 40");
-                nombreB=false;
-            }else if(entrada[1].value.length==0 || entrada[1].value.length>99999){
-                window.alert("Introduzca un sueldo válido");
-                salarioB=false;
-            }
+    var nombreB = true;
+    var salarioB = true;
+
+    if (entrada[0].value.length < 5 || entrada[0].value.length > 40) {
+        window.alert("Minimo 5 carácteres y max 40");
+        nombreB = false;
+    } else if (entrada[1].value.length == 0 || entrada[1].value.length > 99999) {
+        window.alert("Introduzca un sueldo válido");
+        salarioB = false;
+    }
 
 
-        }
+}
 
